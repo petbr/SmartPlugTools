e522ca312b18e67dc73b7698271d4eb712c0953a
Peter Brude <peter.brude@gmail.com>
2019-02-24 14:53:20 +0100

    Final look for Run4ever

--- Run4ever.sh
+++ Run4ever.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 while true; do
-	#python test.py -t 192.168.1.18 -c energy | tee /tmp/ful_log.txt
 	touch /tmp/DranpumpData/RestartIndicator
-	top
+	python test.py -t 192.168.1.18 -c energy | tee /tmp/ful_log.txt
 done
