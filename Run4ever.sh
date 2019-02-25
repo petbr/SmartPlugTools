447ea769540dca525ec123bdb654d1739f675138
Peter Brude <peter.brude@gmail.com>
2019-02-25 18:12:48 +0100

    Log ful_log.txt in correct directory

--- Run4ever.sh
+++ Run4ever.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
 	touch /tmp/DranpumpData/RestartIndicator
-	python test.py -t 192.168.1.18 -c energy | tee /tmp/ful_log.txt
+	python test.py -t 192.168.1.18 -c energy | tee /tmp/DranpumpData/ful_log.txt
 done
