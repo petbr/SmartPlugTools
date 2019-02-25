e04ddb29face95734a2e9f1218a580bbea8dc94e
Peter Brude <peter@Sonya18.2>
2019-02-25 18:31:19 +0100

    wanted pump time=7, minPw=2, pAbOff=2

--- test.py
+++ test.py
@@ -546,9 +546,9 @@ def startup():
   P_waterPumpingTreshold = 350
 
   # Time tresholds.
-  T_wantedPumpTime          = 6
-  T_minPumpingWater         = 4
-  T_pumpingAirBeforeTurnOff = 4
+  T_wantedPumpTime          = 7
+  T_minPumpingWater         = 2
+  T_pumpingAirBeforeTurnOff = 2
   T_reportAfterOffTime      = 2
   T_defaultMaxOffTime       = 200
   T_maxOffTime              = 10
