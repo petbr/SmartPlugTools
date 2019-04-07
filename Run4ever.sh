7afc9f670baf477e88ec67362beab45896d7d65a
Peter Brude <peter@Sonya18.2>
2019-04-07 08:24:14 +0200

    Time stamp RestartIndicator

--- Run4ever.sh
+++ Run4ever.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 while true; do
-	touch /tmp/DranpumpData/RestartIndicator
+  curTime=$(date "+%Y%m%d_%H%M%S")
+  echo "Current Time: $curTime"
+  newFileName=RestartIndicator_$curTime.txt
+  echo "New FileName: "$newFileName
+  
+  sleep 5
+	touch /tmp/DranpumpData/$newFileName
 	python test.py -t 192.168.1.18 -c energy | tee /tmp/DranpumpData/ful_log.txt
 done
