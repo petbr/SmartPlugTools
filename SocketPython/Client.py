6fb808201a99534de3f7e1c68d2359d2f52ae3bd
Peter Brude <peter.brude@gmail.com>
2020-11-15 15:59:31 +0100

    Socket #2

--- SocketPython/Client.py
+++ SocketPython/Client.py
@@ -1,18 +1,31 @@
 import socket
+import sys
 
+def parseArgs():
+    sHost        = sys.argv[1]
+    port         = int(sys.argv[2])
+    nrIters      = int(sys.argv[3])
+    printEachNth = int(sys.argv[4])
+    print("F sHost      = ", sHost)
+    print("F port      = ", port)
+    print("F nrIters   = ", nrIters)
+    print("F printEach = ", printEachNth)
+    return (sHost, port, nrIters, printEachNth)
 
 def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    print("Host = " + host)
-    host = "192.168.1.80"
-    print("Host = " + host)
-    port = 5000  # socket server port number
+    (sHost, port, nrIters, printEach) = parseArgs()
+
+    #host = socket.gethostname()  # as both code is running on same pc
+    print("sHost = " + sHost)
+    #host = "192.168.1.80"
+    print("sHost = " + sHost)
+    #port = 5000  # socket server port number
 
     client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
+    client_socket.connect((sHost, port))  # connect to the server
 
-    message = input(" -> ")  # take input
-
+    #message = input(" -> ")  # take input
+    message = "Start!"
     i = 0
     while message.lower().strip() != 'bye':
         client_socket.send(message.encode())  # send message
@@ -22,10 +35,10 @@ def client_program():
         #message = input(" -> ")  # again take input
         message = "Kalle" + str(i)
         i = i + 1
-        if i == 100000:
+        if i == nrIters:
             message = "bye"
 
-    print('Sent to  server, number of calls: ' + i)  # show in terminal
+    print('Sent to  server, number of calls: ' + str(i))  # show in terminal
     client_socket.close()  # close the connection
 
 
--- SocketPython/Server.py
+++ SocketPython/Server.py
@@ -1,15 +1,32 @@
 import socket
+import sys
 
+def parseArgs():
+    sHost        = sys.argv[1]
+    port         = int(sys.argv[2])
+    nrIters      = int(sys.argv[3])
+    printEachNth = int(sys.argv[4])
+    print("F sHost      = ", sHost)
+    print("F port      = ", port)
+    print("F nrIters   = ", nrIters)
+    print("F printEach = ", printEachNth)
+    return (sHost, port, nrIters, printEachNth)
 
 def server_program():
+    (sHost, port, nrIters, printEach) = parseArgs()
+    print("S sHost      = ", sHost)
+    print("S port      = ", port)
+    print("S nrIters   = ", nrIters)
+    print("S printEach = ", printEach)
+
     # get the hostname
     #host = socket.gethostname()
-    host = "192.168.1.80"
-    port = 5000  # initiate port no above 1024
+    #host = "192.168.1.80"
+    #port = 5000  # initiate port no above 1024
 
     server_socket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
+    server_socket.bind((sHost, port))  # bind host address and port together
 
     # configure how many client the server can listen simultaneously
     server_socket.listen(2)
@@ -27,7 +44,7 @@ def server_program():
         i = i + 1
         conn.send(data.encode())  # send data to the client
 
-    print('Received from client, number of calls: ' + i)  # show in terminal
+    print('Received from client, number of calls: ' + str(i))  # show in terminal
     conn.close()  # close the connection
 
 
