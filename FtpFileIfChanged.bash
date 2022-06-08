fd94be1aa4f23ba76b8eb411c256f2033e4c2e58
Peter Brude <peter.brude@gmail.com>
2022-06-08 18:32:43 +0200

    SQ #1

--- FtpFileIfChanged.bash
+++ FtpFileIfChanged.bash
@@ -8,24 +8,46 @@ echo "Pwd: $pwd"
 m1=""
 echo $m1
 
+echo ---------- Starting FTP Thing >> /var/log/DranpumpData/TheThing.log
+date  >> /var/log/DranpumpData/TheThing.log
+echo Start FTP  >> /var/log/DranpumpData/TheThing.log
 
+mlog1=""
 
 while true; do
-
   # md5sum is computationally expensive, so check only once every 10 seconds
   sleep 20
 
   m2=$(md5sum "$filename")
+  mlog2=$(md5sum /var/log/DranpumpData/TheThing.log)
 
   if [ "$m1" != "$m2" ] ; then
+    echo ---------- FTP Thing >> /var/log/DranpumpData/TheThing.log
+    date  >> /var/log/DranpumpData/TheThing.log
+    echo FTP Sending new DATA file  >> /var/log/DranpumpData/TheThing.log
+
     echo $m2
     date
     date
-    echo "File is changed, time to FTP file!"
+    echo "Data file is changed, time to FTP file!"
     date
     date
     echo $m1
     m1=$m2
     python3.5 SendFileToFtp.py $sajt $user $pwd
   fi
+
+
+  if [ "$mlog1" != "$mlog2" ] ; then
+    echo $mlog2
+    date
+    date
+    echo "Log file is changed, time to FTP file!"
+    date
+    date
+    echo $mlog1
+    mlog1=$mlog2
+    python3.5 SendLogToFtp.py $sajt $user $pwd
+  fi
+
 done
---
+++ reboot-wifi-lost.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# see https://github.com/HankB/reboot-wifi-lost/blob/main/README.md for
+# installation and usage notes
+
+set -e # exit on error
+set -u # exit on unset variable
+
+if [ "$*""" = "" ] ; then
+    echo "Usage $0 IP"
+    exit 1
+fi
+
+echo #1
+
+count=0
+ping_delay=20
+ping_repeat=5
+
+echo #2
+
+echo ---------- Reboot surveillance >> /var/log/DranpumpData/TheThing.log
+echo ---------- Wait five minutes until GO! >> /var/log/DranpumpData/TheThing.log
+date >> /var/log/DranpumpData/TheThing.log
+
+echo #3
+
+sleep 300
+
+echo #4
+
+echo >> /var/log/DranpumpData/TheThing.log
+echo >> /var/log/DranpumpData/TheThing.log
+echo ---------- Reboot surveillance NOW Starts>> /var/log/DranpumpData/TheThing.log
+date >> /var/log/DranpumpData/TheThing.log
+echo ----------- NOW.....Start Reboot surveillance!!! >> /var/log/DranpumpData/TheThing.log
+echo >> /var/log/DranpumpData/TheThing.log
+
+# ping the target the desired number of times. If 'ping' is successful,
+# the 'let count="0"' executres, returns a non zero exit code and the 
+# script exits due to 'set -e'. If all 'ping' operations fail, the host
+# reboots.
+while [ $count -lt "$ping_repeat" ] ; do
+
+    sleep "$ping_delay"
+    # shellcheck disable=SC2015 # SC2015 is intended logic
+    ping -c1 "$1" &> /dev/null && (( count="0" )) || (( count="$count + 1" ))
+done
+
+echo ---------- The shit is not ping responding EXIT in five minutes after copying log file >> /var/log/DranpumpData/TheThing.log
+date >> /var/log/DranpumpData/TheThing.log
+echo ----------- Let us reboot >> /var/log/DranpumpData/TheThing.log
+
+cat /var/log/DranpumpData/TheThing.log >> /home/pi/TheThing.log
+
+sleep 300
+
+#reboot
+sudo reboot
