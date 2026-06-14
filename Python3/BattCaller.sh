8456b5f1dc21870107d630657894d6c73678f27c
Peter Brude <peter.brude@gmail.com>
2026-06-14 07:54:18 +0200

    Delta...

--- Python3/BattCaller.sh
+++ Python3/BattCaller.sh
@@ -9,6 +9,6 @@ while true; do
     cat /tmp/theBatt.txt
     
     python /home/pi/repo/SmartPlugTools/Python3/theSleeper.py
-    # Vänta 60 sekunder (så att scriptet inte äter upp all CPU)
-    sleep 60
+    # Vänta 1200 sekunder (så att scriptet inte äter upp all CPU)
+    sleep 1200
 done
