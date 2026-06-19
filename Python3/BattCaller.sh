83b47ae4efe072af442b26192b971a9360de7ac4
Peter Brude <peter.brude@gmail.com>
2026-06-19 08:04:17 +0200

    5 min. per run

--- Python3/BattCaller.sh
+++ Python3/BattCaller.sh
@@ -7,8 +7,8 @@ echo "Startar loop the Sleeper"
 while true; do
     # Skriver ut innehållet i filen till terminalen
     cat /tmp/theBatt.txt
-    
+
     python /home/pi/repo/SmartPlugTools/Python3/theSleeper.py
-    # Vänta 1200 sekunder (så att scriptet inte äter upp all CPU)
-    sleep 60
+    # Vänta 300 sekunder (så att scriptet inte äter upp all CPU)
+    sleep 300
 done
