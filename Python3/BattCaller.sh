16ac7164ba5e428349dc29ad95f491c82917fd59
Peter Brude <peter.brude@gmail.com>
2026-06-20 10:50:09 +0200

    June 20

--- Python3/BattCaller.sh
+++ Python3/BattCaller.sh
@@ -8,7 +8,7 @@ while true; do
     # Skriver ut innehållet i filen till terminalen
     cat /tmp/theBatt.txt
 
-    python /home/pi/repo/SmartPlugTools/Python3/theSleeper.py
+    timeout -k 102 120 python /home/pi/repo/SmartPlugTools/Python3/theSleeper.py
     # Vänta 1200 sekunder (så att scriptet inte äter upp all CPU)
     sleep 1200
 done
